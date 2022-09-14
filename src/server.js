5e06ec331cb45f9b7aaf32cf6b5faeb9c92ef037
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2022-09-14 19:00:02 +0700

    rollback

--- src/server.js
+++ src/server.js
@@ -8,8 +8,7 @@ const init = async () => {
 
   const server = Hapi.server({
     port: 5000,
-    host:
-      process.env.NODE_ENV !== 'production' ? 'localhost' : '13.212.236.144',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
